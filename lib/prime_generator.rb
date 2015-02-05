4c7c8331aaaabcb5aebada7356ddd04e982db6d3
Carlos Martinez <carlos.martinez@forward.co.uk>
2015-02-05 22:01:09 +0000

    simplistic implementation of prime numbers

--- lib/prime_generator.rb
+++ lib/prime_generator.rb
@@ -1,4 +1,16 @@
 class PrimeGenerator
   def prime(i)
+    primes[i-1]
   end
+
+  private
+
+  def primes
+    pool = (2..1000).to_a
+    pool.map do |i| 
+      (i*2 .. pool.max).step(i).each {|j| pool.delete j}
+      i      
+    end
+  end
+
 end
--- spec/prime_generator_spec.rb
+++ spec/prime_generator_spec.rb
@@ -8,4 +8,24 @@ describe PrimeGenerator do
     expect(prime_generator.prime(1)).to eq 2
   end
 
+  it "returns 2nd prime" do
+    expect(prime_generator.prime(2)).to eq 3
+  end
+
+  it "returns 3rd prime" do
+    expect(prime_generator.prime(3)).to eq 5
+  end
+
+  it "returns 5th prime" do
+    expect(prime_generator.prime(5)).to eq 11
+  end
+
+  it "returns 10th prime" do
+    expect(prime_generator.prime(10)).to eq 29
+  end
+
+  it "returns 100th prime" do
+    expect(prime_generator.prime(100)).to eq 541
+  end
+
 end
